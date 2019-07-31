1ed3886cb7c7dce8669c747b8014d2b4255fce34
107124 <kredfox4@gmail.com>
2019-07-31 16:30:19 -0600

    Added About Page

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Donut1 from "../images/donut1.png";
+
+export default class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <img src={Donut1} alt="Donut Image" />
+      </div>
+    );
+  }
+}
