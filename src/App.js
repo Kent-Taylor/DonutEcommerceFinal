4c5ffbe7bb2133e60818977f245574fe8dbc5354
Ethan Deans <deanseth0000@gmail.com>
2019-08-02 11:56:25 -0600

    made homepage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 import Heading from "./components/heading";
 import Footer from "./components/footer";
+import Homepage from "./pages/homepage";
 
 function App() {
   return (
@@ -11,16 +12,7 @@ function App() {
         <Heading />
       </div>
       <div className="page-container" />
-      <div className="donut-background">
-        <img
-          src="https://images-gmi-pmc.edge-generalmills.com/9c8e5dc4-2883-4fff-97b2-ed778822c2e5.jpg"
-          alt="donut-pic"
-        />
-      </div>
-      <div className="todays-suggestions">
-        <h1>Today's Suggestions</h1>
-        <div className="suggestions">Suggestions go here</div>
-      </div>
+      <Homepage />
       <div>
         <Footer />
       </div>
---
+++ src/pages/homepage.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div className="homepage-container">
+      <div className="donut-background">
+        <img
+          src="https://images-gmi-pmc.edge-generalmills.com/9c8e5dc4-2883-4fff-97b2-ed778822c2e5.jpg"
+          alt="donut-pic"
+        />
+      </div>
+      <div className="todays-suggestions">
+        <h1>Today's Suggestions</h1>
+        <div className="suggestions">Suggestions go here</div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
