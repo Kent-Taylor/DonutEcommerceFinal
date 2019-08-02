cd2fe1c9252eb813657a0e5ed0716ceefdfabf2c
gabrielaMendez <ella.mendez1127@gmail.com>
2019-08-02 12:08:03 -0500

    login styles added

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -34,35 +34,42 @@ export default class Login extends Component {
   render() {
     return (
       <div className="auth-form-wrapper">
-        <h1>LOGIN TO ADMIN ACCOUNT</h1>
+        <div className="elements-in-form-wrapper">
+          <h1>Admin Account Login</h1>
 
-        <form onSubmit={this.handleSubmit} className="login-form-wrapper">
-          <div className="form-group">
-            <FontAwesomeIcon icon="envelope" />
-            <input
-              type="email"
-              name="email"
-              placeholder="Your email"
-              value={this.state.email}
-              onChange={this.handleChange}
-            />
-          </div>
+          <form onSubmit={this.handleSubmit} className="login-form">
+            <div className="form-group-wrapper">
+              <div className="form-group">
+                <div className="font-awesome-icon">
+                  <FontAwesomeIcon icon="envelope" />
+                </div>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Your email"
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                />
+              </div>
 
-          <div className="form-group">
-            <FontAwesomeIcon icon="lock" />
-            <input
-              type="password"
-              name="password"
-              placeholder="Your password"
-              value={this.state.password}
-              onChange={this.handleChange}
-            />
-          </div>
-
-          <div>
-            <button type="submit">Login</button>
-          </div>
-        </form>
+              <div className="form-group">
+                <div className="font-awesome-icon">
+                  <FontAwesomeIcon icon="lock" />
+                </div>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Your password"
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                />
+              </div>
+            </div>
+            <div className="login-button">
+              <button type="submit">Login</button>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
